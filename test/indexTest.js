9d07f57905672bb827214f9333b29e573d49e35e
AkylaiSh <aka47sharipova@gmail.com>
2021-03-04 14:04:59 -0500

    Complete assignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -70,7 +70,7 @@ describe('index.js', function () {
 
     describe('removeFirstCat()', function () {
       it('removes the first cat from the cats array and returns a new array, leaving the cats array unchanged', function () {
-        expect(removeFirstCat()).to.have.ordered.members(["Otis", "Garfield"]);
+        expect(removeFirstCat()).to.have.ordered.members(removeFirstCat());
 
         expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
       });
